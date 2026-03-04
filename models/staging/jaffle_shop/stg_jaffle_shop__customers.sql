39e302921d31ba063fa06d5754a7408673c1b035
Jerocelle Gingo <jerocellegingo@gmail.com>
2026-03-04 17:28:28 +0000

    Updated depreciation freshness config under tables. Added generic test

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,7 @@
     select
         id as customer_id,
         first_name,
-        last_name
+        last_name,
+        _etl_loaded_at
 
     from {{ source('jaffle_shop', 'customers') }}
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,9 +3,8 @@ select
     orderid AS order_id,
     paymentmethod AS payment_method,
     status,
-
     --amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created AS created_at
 
-from raw.stripe.payment
+from {{ source('stripe', 'payment') }}
